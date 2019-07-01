197582c0f678bb2b437e624c5ab35b9e16f75bb1
gokulnath729 <gokulnath729@gmail.com>
2019-07-01 15:22:19 +0530

    first commit

---
+++ precache-manifest.520c5acec60c6bfb35a989908cf9bb2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c48ec8328323b4c34965030d7d69368f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b443cceab4ea0c3349e",
+    "url": "/static/css/main.576a3f5e.chunk.css"
+  },
+  {
+    "revision": "276047129b7b91167b0b",
+    "url": "/static/js/2.93bebb86.chunk.js"
+  },
+  {
+    "revision": "3b443cceab4ea0c3349e",
+    "url": "/static/js/main.4d258342.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
